ee3eea5b11c6604b6dc01a7b4a90fb82de27861e
jserweta <jakub.serweta@gmail.com>
2025-02-27 16:04:37 +0100

    replace chakra-ui with shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Provider } from "@/components/ui/provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} `}>
-        <Provider>{children}</Provider>
+        {children}
       </body>
     </html>
   );
