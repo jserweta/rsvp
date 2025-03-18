65140b2a869c4299266027ce94c7b08930e69439
jserweta <jakub.serweta@gmail.com>
2025-03-18 20:05:43 +0100

    fix build errors

--- src/lib/dashboard/actions.ts
+++ src/lib/dashboard/actions.ts
@@ -99,54 +99,54 @@ const CreateUser = FormSchemaCreateUser.omit({ id: true }).refine(
 //   redirect("/dashboard/invoices");
 // }
 
-export async function updateGuest(
-  id: string,
-  prevState: State,
-  formData: FormData
-) {
-  // Validate form using Zod
-  const validatedFields = UpdateInvoice.safeParse({
-    customerId: formData.get("customerId"),
-    amount: formData.get("amount"),
-    status: formData.get("status"),
-  });
+// export async function updateGuest(
+//   id: string,
+//   prevState: State,
+//   formData: FormData
+// ) {
+//   // Validate form using Zod
+//   const validatedFields = UpdateInvoice.safeParse({
+//     customerId: formData.get("customerId"),
+//     amount: formData.get("amount"),
+//     status: formData.get("status"),
+//   });
 
-  // If form validation fails, return errors early. Otherwise, continue.
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-      message: "Missing Fields. Failed to Create Invoice.",
-    };
-  }
+//   // If form validation fails, return errors early. Otherwise, continue.
+//   if (!validatedFields.success) {
+//     return {
+//       errors: validatedFields.error.flatten().fieldErrors,
+//       message: "Missing Fields. Failed to Create Invoice.",
+//     };
+//   }
 
-  const { customerId, amount, status } = validatedFields.data;
+//   const { customerId, amount, status } = validatedFields.data;
 
-  const amountInCents = amount * 100;
+//   const amountInCents = amount * 100;
 
-  try {
-    await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
-  } catch (error) {
-    return { message: "Database Error: Failed to Update Invoice." };
-  }
+//   try {
+//     await sql`
+//     UPDATE invoices
+//     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+//     WHERE id = ${id}
+//   `;
+//   } catch (error) {
+//     return { message: "Database Error: Failed to Update Invoice." };
+//   }
 
-  revalidatePath("/dashboard/invoices");
-  redirect("/dashboard/invoices");
-}
+//   revalidatePath("/dashboard/invoices");
+//   redirect("/dashboard/invoices");
+// }
 
-export async function deleteInvoice(id: string) {
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath("/dashboard/invoices");
+// export async function deleteInvoice(id: string) {
+//   try {
+//     await sql`DELETE FROM invoices WHERE id = ${id}`;
+//     revalidatePath("/dashboard/invoices");
 
-    return { message: "Deleted Invoice." };
-  } catch (error) {
-    return { message: "Database Error: Failed to Delete Invoice." };
-  }
-}
+//     return { message: "Deleted Invoice." };
+//   } catch (error) {
+//     return { message: "Database Error: Failed to Delete Invoice." };
+//   }
+// }
 
 export async function authenticate(
   prevState: string | undefined,
