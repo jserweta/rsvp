8e64749243eb1c3f46df2e02ed29908a9861f23c
jserweta <jakub.serweta@gmail.com>
2025-03-25 15:50:02 +0100

    add new password requirements

--- src/lib/schema/signUpForm.ts
+++ src/lib/schema/signUpForm.ts
@@ -4,7 +4,19 @@ export const SignUpFormSchema = z.object({
   id: z.string(),
   name: z.string().min(1, { message: "Name is required" }),
   email: z.string().email(),
-  password: z.string().min(8),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .regex(/\d/, { message: "Password must contain at least one number" })
+    .regex(/[^A-Za-z0-9]/, {
+      message: "Password must contain at least one special character",
+    }),
   passwordConfirm: z.string(),
 });
 
