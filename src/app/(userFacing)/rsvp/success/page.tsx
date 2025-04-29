4bdeaa7cd1b4663e2eb3e656edd1c239c9f187d7
jserweta <jakub.serweta@gmail.com>
2025-04-29 09:40:38 +0200

    add btn on success page

--- src/app/(userFacing)/rsvp/success/page.tsx
+++ src/app/(userFacing)/rsvp/success/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import Header from '@/components/header';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 
@@ -18,21 +20,34 @@ function SuccessPage() {
 
   let subTitle = '';
   let title = '';
+  let showDetailsBtn = false;
   switch (status) {
     case 'confirmed':
       title = 'Dziękujemy za potwierdzenie!';
       subTitle = 'Do zobaczenia na weselu! Cieszymy się, że będziecie z nami.';
+      showDetailsBtn = true;
       break;
     case 'submitted':
       title = 'Otrzymaliśmy już Waszą odpowiedź!';
       subTitle = 'Jeśli chcielibyście coś zmienić prosimy o kontakt.';
+      showDetailsBtn = true;
       break;
     default:
       title = 'Dziękujemy za wiadomość!';
       subTitle =
         'Szkoda, że nie możecie być z nami – mamy nadzieję, że spotkamy się przy innej okazji.';
+      showDetailsBtn = false;
       break;
   }
 
-  return <Header title={title} subTitle={subTitle} />;
+  return (
+    <>
+      <Header title={title} subTitle={subTitle} />
+      {showDetailsBtn && (
+        <Button asChild>
+          <Link href={'/'}>Szczegóły uroczystości</Link>
+        </Button>
+      )}
+    </>
+  );
 }
